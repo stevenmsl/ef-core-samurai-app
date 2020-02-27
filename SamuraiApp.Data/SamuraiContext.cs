22ab5c2519953f8300e4cbe9bb9c386f46ae4994
Steven Lin <slin@merchantgrowth.com>
2020-02-27 13:14:41 -0800

    add migrations

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -9,10 +9,24 @@ namespace SamuraiApp.Data
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Clan> Clans { get; set; }
 
+        public DbSet<Battle> Battles { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(
                 @"Server=.\sqlexpress;Database=SamuraiAppData;Trusted_Connection=True;");           
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SamuraiBattle>().HasKey(s => new { s.SamuraiId, s.BattleId });
+            // as there is no db set declared for horse entity to prevent client code 
+            // from interacting with horses directly the table will be defaulted to Horse (singular), 
+            // use ToTable to affect the name to Horses (plural).            
+            modelBuilder.Entity<Horse>().ToTable("Horses");
+        }
+
+
+
     }
 }
